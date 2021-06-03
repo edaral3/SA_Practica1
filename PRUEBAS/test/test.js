00193bf83becede1c305fa0d5e096a668f1c2446
Edgar Aldana <arnoldo201@hotmail.com>
2021-06-03 12:16:41 -0600

    test terminados

---
+++ PRUEBAS/test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Test', function() {
+  describe('Prueba 1', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
