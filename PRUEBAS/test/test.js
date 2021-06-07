58e853dc097b611cc6d832ffb8eb401941749b46
Edgar Aldana <arnoldo201@hotmail.com>
2021-06-07 13:24:51 -0600

    test

--- PRUEBAS/test/test.js
+++ PRUEBAS/test/test.js
@@ -3,6 +3,6 @@ describe('Test', function() {
   describe('Prueba 1', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(1, 1);
-    });
+    }); 
   });
 }); 
