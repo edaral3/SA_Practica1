63a921d07fb29b84d555c8742607f405568aa65d
Edgar Aldana <arnoldo201@hotmail.com>
2021-06-11 11:22:52 -0600

    test

--- fabric.py
+++ fabric.py
@@ -3,6 +3,6 @@ def function():
     connect_kwargs = {"key_filename":['key.pem']}
     c = Connection(host='18.118.27.241',user='ubuntu',connect_kwargs=connect_kwargs)
     result = c.run("sudo docker login -u edaral3 -p 123456789")
-    result = c.run("sudo docker pull sapractica2:latest")
+    result = c.run("sudo docker pull edaral3/sapractica2:latest")
     result = c.run("sudo docker-compose up -d")
 function()
