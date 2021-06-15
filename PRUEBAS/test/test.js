c6a6f0cd394b177f82644f519fe08611cefb5f37
Edgar Aldana <arnoldo201@hotmail.com>
2021-06-15 13:37:18 -0600

    test fallido

--- PRUEBAS/test/test.js
+++ PRUEBAS/test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Test', function() {
   describe('Prueba 1', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(1, 1);
+      assert.equal(1, 2);
     }); 
   });
 }); 
